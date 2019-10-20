fd3d11ed9e2f2136ddeda693b16cfcddac57b307
Junhwi So <sjha24@naver.com>
2019-10-20 15:25:03 +0900

    10.20 연습

--- 10818 최소, 최대.py
+++ 10818 최소, 최대.py
@@ -1,3 +1,14 @@
+https://www.acmicpc.net/problem/10818
+
 from sys import stdin as std
 
-cnt = std.readline()
+cnt = int(std.readline())
+a = std.readline().split()
+
+for i in range(cnt):
+    a[i] = int(a[i])
+
+print(min(a),max(a))
+
+
+
---
+++ 2920 음계.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/2920
+
+a = input().split()
+if a == sorted(a):
+    print('ascending')
+elif a == sorted(a,reverse = True):
+    print('descending')
+else:
+    print('mixed')
